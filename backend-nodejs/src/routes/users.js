4e256e44bc0dcd7e40d90b77f6775025cdf88f88
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2024-07-05 17:42:18 +0900

    database 연동

--- backend-nodejs/src/routes/users.js
+++ backend-nodejs/src/routes/users.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../database');
+const db = require("../database");
 
-router.get('/', async (req, res) => {
-  const [rows] = await db.query('SELECT * FROM users');
+router.get("/", async (req, res) => {
+  const [rows] = await db.query("SELECT * FROM userlogin");
+  res.json(rows);
+});
+
+router.get("/sleepInfo/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  const [rows] = await db.query(
+    "SELECT * FROM userSleepInfo WHERE user_id = ?",
+    [userId]
+  );
   res.json(rows);
 });
 
