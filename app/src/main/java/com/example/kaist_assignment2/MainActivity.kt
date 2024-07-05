34b173237375ff97bcb3c0ed2b827c803e11c16a
jimini1026 <128363259+jimini1026@users.noreply.github.com>
2024-07-05 22:17:18 +0900

    로그인 디자인 수정

--- app/src/main/java/com/example/kaist_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/kaist_assignment2/MainActivity.kt
@@ -2,38 +2,19 @@ package com.example.kaist_assignment2
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.kaist_assignment2.ui.theme.Kaist_Assignment2Theme
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Kaist_Assignment2Theme {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Hello World")
+        setContentView(R.layout.activity_main)
 
-                    Button(
-                        onClick = {
-                            val intent = Intent(this@MainActivity, AuthCodeHandlerActivity::class.java)
-                            startActivity(intent)
-                        },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 16.dp)
-                    ) {
-                        Text(text = "Login with Kakao")
-                    }
-                }
-            }
+        val kakaoLoginButton: Button = findViewById(R.id.kakao_login_button)
+        kakaoLoginButton.setOnClickListener {
+            val intent = Intent(this, AuthCodeHandlerActivity::class.java)
+            startActivity(intent)
         }
     }
 }
